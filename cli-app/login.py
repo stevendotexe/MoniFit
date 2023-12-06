142e2dad516d8905056282794d9f7ce8f7b4db4e
Steven Martua Christian Simbolon <steven@upi.edu>
2023-12-06 15:55:15 +0700

    user data

--- cli-app/login.py
+++ cli-app/login.py
@@ -1,2 +1,19 @@
-import csv
 
+def register(): # Register user
+    email = input("Masukkan email: ")
+
+    while True:
+        password = input("Masukkan password")
+        if password >= 8:
+            break
+        else:
+            print("Password harus lebih dari 8 karakter.")
+
+    user_info[email] = password
+
+def login(): # login user
+    email = input("Masukkan email: ")
+    while True:
+        if email not in user_info:
+            selection = (input("Email tidak ada dalam database. apakah anda ingin registrasi? (1 coba ulang, 2 lupa password, 3 registrasi)"))
+            if selection
