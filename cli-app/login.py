d4790c01e80fc2df6902e111e86cc2deedde463b
Steven Martua Christian Simbolon <steven@upi.edu>
2023-12-12 17:30:50 +0700

    username availability fix

--- cli-app/login.py
+++ cli-app/login.py
@@ -3,35 +3,47 @@ import os
 import json
 
 def main():
-    pass
+    while True:
+        try:
+            selection = input("Pilih mode: \n1. Registrasi\n2. Login\n")
+        except ValueError:
+            print("Mohon pilih nomor antara 1 atau 2.")
+        if selection == 1:
+            register()
+            break
+        elif selection == 2:
+            login()
+            break
+        else: pass
 
 # Function to register a new user
 def register():
     email = input("Masukkan email: ")
     day = date.today()
     usernames = [user.rstrip('.json') for user in os.listdir("userinfo")]
-    while True:
-        while True:
-            username = input("Masukkan username: ")
-            if username in usernames and not username.isalnum():
-                print(f"Username dengan nama {username} sudah ada atau tidak alfanumerik.")
-            else:
-                break
-        password = input("Masukkan password: ")
-        if len(password) >= 8:   
-            with open(f'userinfo/{username}.json', 'w') as file:
-                file.write("{\n")
-                file.write(f'"username": "{username}",\n')
-                file.write(f'"password": "{password}",\n')
-                file.write(f'"email": "{email}",\n')
-                file.write(f'"registration_date": "{day}",\n')
-                file.write('"weight_history": ' + '{'+f'"{day}":' +  '0' + '}\n')
-                file.write("}\n")
-            print("Registrasi sukses!")
-            break
-        else:
-            print("Password harus lebih dari 8 karakter.")
+    print(usernames)
 
+    while True:
+        username = input("Masukkan username: ")
+        if username in usernames or not username.isalnum():
+            print(f"Username dengan nama {username} sudah ada atau tidak alfanumerik.")
+        else:
+            while True:
+                password = input("Masukkan password: ")
+                if len(password) >= 8:   
+                    with open(f'userinfo/{username}.json', 'w') as file:
+                        file.write("{\n")
+                        file.write(f'"username": "{username}",\n')
+                        file.write(f'"password": "{password}",\n')
+                        file.write(f'"email": "{email}",\n')
+                        file.write(f'"registration_date": "{day}",\n')
+                        file.write('"weight_history": ' + '{'+f'"{day}":' +  '0' + '}\n')
+                        file.write("}\n")
+                    print("Registrasi sukses!")
+                    break
+                else:
+                    print("Password harus lebih dari 8 karakter.")
+            break
 def login():
     usernames = [user.rstrip('.json') for user in os.listdir("userinfo")]
     while True:
@@ -39,6 +51,8 @@ def login():
         if username in usernames:
             with open(f'{username}.json', 'r'):
                 pass
+        else:
+            print(f"Username dengan nama {username} tidak ditemkuan. Apakah anda ingin registrasi?")
 
 if __name__ == '__main__':
     register()
